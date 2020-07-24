9738326e83dc5fe49e874770d1b1c65984518d33
Gustavo Banegas <gustavo@cryptme.in>
2020-07-24 11:35:13 +0200

    added less users

--- add_vhss_tss/params.h
+++ add_vhss_tss/params.h
@@ -5,7 +5,7 @@
 #define FINITE_FIELD 8068725504281211481
 #define SECURITY  64
 #define G 3
-#define NR_CLIENTS  1100
+#define NR_CLIENTS  100
 #define NR_SERVERS  4
 #define THRESHOLD_CLIENTS  2
 #define THRESHOLD 3
